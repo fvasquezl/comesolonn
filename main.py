6c2f0fc3be35b6f26fa494a2272b98aa7e67ad7c
fvasquezl <vasquez.limon@gmail.com>
2024-10-24 14:58:53 -0700

    reglas horizontales

--- main.py
+++ main.py
@@ -11,16 +11,24 @@ class Rule:
         grand = node + row + inc2
         self.add_child([node, child, grand+row])
 
+    def find_horizontal(self, lst, chunk_size):
+        sublists = [lst[i:i + chunk_size] for i in range(0, len(lst), 1) if len(lst[i:i + chunk_size]) == chunk_size]
+        for x in sublists:
+            self.add_child(x)
+
     def __repr__(self):
         return str(self.rule)
 
     def child(self):
-        for i in range(5-2):
+        for i in range(5):
             starPosition = (i * (i + 1)) // 2
             endPosition = starPosition + i
-            for j in range(starPosition, endPosition + 1):                
-                self.find_child(j, i, 1, 3) # find left child 
-                self.find_child(j, i, 2, 5) # find right child
+            array_range = range(starPosition, endPosition + 1)
+            if i < 3:
+                for j in array_range:                
+                    self.find_child(j, i, 1, 3) # find left child 
+                    self.find_child(j, i, 2, 5) # find right child
+            self.find_horizontal([*array_range], 3) # find horizontal child
 
 if __name__ == "__main__":
     rule = Rule()
